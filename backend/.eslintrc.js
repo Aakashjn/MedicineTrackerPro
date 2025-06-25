495bcdbfa5f82fdbd1ce583bd23d34202bea5d92
Aakash <147521183+Aakashjn@users.noreply.github.com>
2025-06-25 19:16:52 +0530

    Update .eslintrc.js

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,21 +1,36 @@
-// backend/.eslintrc.js
 module.exports = {
-    "env": {
-        "commonjs": true,
-        "es2021": true,
-        "node": true,
-        "jest": true
-    },
-    "extends": [
-        "eslint:recommended"
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest"
-    },
-    "rules": {
-        "no-console": "off",
-        // UPDATED: Added 'req|res|next' to argsIgnorePattern
-        "no-unused-vars": ["warn", { "argsIgnorePattern": "^_|req|res|next" }],
-        "no-undef": "error"
-    }
+  env: {
+    browser: false,
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jest: true
+  },
+  extends: [
+    'eslint:recommended',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  rules: {
+    // Custom rules for your project
+    'no-console': 'warn',
+    'no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
+    'semi': ['error', 'always'],
+    'quotes': ['error', 'single'],
+    'indent': ['error', 2],
+    'no-trailing-spaces': 'error',
+    'eol-last': 'error',
+    'comma-dangle': ['error', 'never'],
+    'space-before-function-paren': ['error', 'never']
+  },
+  ignorePatterns: [
+    'node_modules/',
+    'coverage/',
+    'dist/',
+    'build/',
+    '*.min.js'
+  ]
 };
